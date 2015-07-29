7570e45d707601f8f5dd58a8ba0c65d393e58905
shaojf <shaojf@yiche.com>
2015-07-29 11:12:28 +0800

    Initial Commit

---
+++ AnimationBtnDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AnimationBtnDemo
+//
+//  Created by Seven on 7/29/15.
+//  Copyright (c) 2015 Seven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
